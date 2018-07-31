d8cc1e8598c509d4b4de260a8fe6ef7fdabd1336
gregoryls1 <contatogregory@gmail.com>
2018-07-31 17:04:16 -0300

    melhorias na validação do campo nome no formulario

--- js/valida_form.js
+++ js/valida_form.js
@@ -9,14 +9,6 @@ function validaNome(nome){
        
 function isTipo(nome){
         var nome = coletaDados(nome);
-		var reTipo = /[A-z][ ][A-z]/;
+		var reTipo = /[A-z][A-z][ ][A-z][A-z]/;
 		return reTipo.test(nome);
 	}
-
-/*function validaEmail(email){
-		var email = coletaDados(email);
-		if (email.lenght > 0)
-			return coletaDados("email");
-		else
-			return ("Por favor preencher e-mail!");
-}*/
